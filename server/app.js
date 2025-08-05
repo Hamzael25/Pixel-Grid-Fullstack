a8a9fe70c04824eab9f4e80a4948081b139b5093
Hamza EL OUARDI <hamzaelouardi@mac.home>
2025-08-05 23:57:51 +0200

    Ajout de la route POST /setGridColor pour modifier la couleur d'une case
    - Middleware express.json() ajouté pour parser les requêtes JSON
    - Nouvelle route POST /setGridColor :
      • Vérifie la validité des coordonnées et de la couleur
      • Met à jour la couleur d’une case spécifique dans la grille
      • Renvoie la grille mise à jour

--- server/app.js
+++ server/app.js
@@ -5,21 +5,48 @@ const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use(cors());
 
 app.listen(PORT, () => {
-  console.log("Welcome in The Pixel Grid Full Stack Project");
+	console.log("Welcome in The Backend of Pixel Grid Full Stack Project");
 });
 
 app.get("/grid", (req, res) => {
-  try {
-    const grid = db.prepare(`SELECT * FROM grid`).all();
-    res.status(200).json({
-      grid: grid,
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: "An Error occured",
-    });
-  }
+
+	try {
+		const grid = db.prepare(`SELECT * FROM grid`).all();
+		res.status(200).json({
+			grid: grid,
+		});
+	} catch (error) {
+		res.status(500).json({
+			error: "Error fetching grid data",
+		});
+	}
+});
+
+app.post("/setGridColor", (req, res) => {
+	const { x, y, color } = req.body;
+	if (!color) {
+		return res.status(400).json({
+			message: "Invalid color value",
+		});
+	}
+
+	const row = db.prepare(`SELECT * FROM grid WHERE x = ? AND y = ?`).get(x, y);
+	if (!row) {
+		return res.status(400).json({
+			message: "Invalid coordinates",
+		});
+	}
+
+	const update = db
+		.prepare(`UPDATE grid SET color = ? WHERE x = ? AND y = ?`)
+		.run(color, x, y);
+	const updatedGrid = db.prepare(`SELECT * FROM grid`).all();
+	res.status(200).json({
+		message: "Grid color updated",
+		grid: updatedGrid,
+	});
 });
