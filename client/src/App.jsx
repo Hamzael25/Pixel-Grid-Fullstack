a9e24501a804423a1ec5ed8b3151114ddb96e203
Hamza EL OUARDI <hamzaelouardi@mac.home>
2025-08-06 01:11:41 +0200

    feat: Ajout de l'interface de sélection de couleur et interaction avec la grille
    - Création du composant Toolbar pour sélectionner une couleur parmi 8 proposées
    - Ajout du state selectedColor dans App.jsx
    - Intégration du POST vers l’API (/setGridColor) pour mettre à jour la couleur d’une cellule
    - Modification de PixelGrid.jsx pour déclencher la mise à jour au clic
    - Création et amélioration des fichiers CSS : effets au survol, responsive mobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import PixelGrid from "./PixelGrid";
+import Toolbar from "./Toolbar";
 
-const URL = "http://localhost:3000"; // Adjust the URL as needed
+const URL = "http://localhost:3000";
 
 const App = () => {
   const [grid, setGrid] = useState([]);
-
+  const [selectedColor, setSelectedColor] = useState("black");
+  const updateColor = async (x, y) => {
+      try {
+        const response = await fetch(`${URL}/setGridColor`, {
+        method : "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ x, y, color: selectedColor })
+      });
+      const {grid : updatedGrid} = await response.json();
+      setGrid(updatedGrid);
+      } catch(error){
+        console.error("Error uptading grid:", error);
+      }
+  }
   useEffect(() => {
     fetch(`${URL}/grid`)
       .then((response) => response.json())
@@ -17,7 +31,12 @@ const App = () => {
   return (
     <div className="content-wrapper">
       <h1>Pixel Grid</h1>
-      <PixelGrid grid={grid} />
+      <PixelGrid 
+        grid={grid} 
+        updateColor={updateColor} />
+      <Toolbar 
+        selectedColor={selectedColor} 
+        setSelectedColor={setSelectedColor} />
     </div>
   );
 };
--- client/src/PixelGrid.jsx
+++ client/src/PixelGrid.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./PixelGrid.css";
 
-const PixelGrid = ({ grid }) => {
+const PixelGrid = ({ grid, updateColor }) => {
   return (
     <div className="grid">
-      {grid.map((cell, index) => (
+      {grid.map((cell) => (
         <div
-          key={index}
+          key={`${cell.x}-${cell.y}`}
           className="gridItem"
           style={{ 
             backgroundColor: cell.color, 
             gridColumnStart: cell.x + 1, 
             gridRowStart: cell.y + 1}}
+		  onClick={() => updateColor(cell.x, cell.y)}
         ></div>
       ))}
     </div>
